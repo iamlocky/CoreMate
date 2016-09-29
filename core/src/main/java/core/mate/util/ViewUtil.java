f185f967f5f8621af865741f0c037f69d31a7461
DrkCore <178456643@qq.com>
2016-09-29 14:42:42 +0800

    方法调整

--- core/src/main/java/core/mate/util/ViewUtil.java
+++ core/src/main/java/core/mate/util/ViewUtil.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.AbsListView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -166,35 +165,6 @@ public final class ViewUtil {
         // listView.getDividerHeight()获取子项间分隔符占用的高度
         // params.height最后得到整个ListView完整显示需要的高度
         listView.setLayoutParams(params);
-    }
-
-	/*获取属性*/
-
-    /**
-     * 获取ListView的滚动的Y。该方法只适用于Item的高度都一致的情况
-     *
-     * @param listView
-     * @return
-     */
-    public static int getListViewScrollY(AbsListView listView) {
-        //这里获得并不是Adapter中第0个item而是ListView可见范围内的第0个item
-        View lastVisibleItem = listView.getChildAt(0);
-        if (lastVisibleItem == null) {
-            return 0;
-        }
-        //初始状态下第0个和firstVisible一样的
-        //但是滑动到下面之后第0个其实是firstVisible的上一个item
-        int firstVisiblePosition = listView.getFirstVisiblePosition();
-        int top = lastVisibleItem.getTop();
-        //综合计算一下就可以得出当前Y，不过只限于Item的高度都是一样的情况下
-        int scrollY = -top + firstVisiblePosition * lastVisibleItem.getHeight();
-
-        if (listView instanceof ListView) {
-            ListView view = (ListView) listView;
-            int dividerHeight = view.getDividerHeight();
-            scrollY += dividerHeight * firstVisiblePosition;
-        }
-        return scrollY;
     }
 
 	/* View截图 */
--- core/src/main/java/core/mate/view/ListScrollBinder.java
+++ core/src/main/java/core/mate/view/ListScrollBinder.java
@@ -2,8 +2,7 @@ package core.mate.view;
 
 import android.view.View;
 import android.widget.AbsListView;
-
-import core.mate.util.ViewUtil;
+import android.widget.ListView;
 
 /**
  * @author DrkCore
@@ -30,7 +29,34 @@ public class ListScrollBinder extends ScrollBinder implements AbsListView.OnScro
 
 	@Override
 	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-		int curScrollY = ViewUtil.getListViewScrollY(view);
+		int curScrollY = getListViewScrollY(view);
 		onScroll(curScrollY);
+	}
+
+	/**
+	 * 获取ListView的滚动的Y。该方法只适用于Item的高度都一致的情况
+	 *
+	 * @param listView
+	 * @return
+	 */
+	public static int getListViewScrollY(AbsListView listView) {
+		//这里获得并不是Adapter中第0个item而是ListView可见范围内的第0个item
+		View lastVisibleItem = listView.getChildAt(0);
+		if (lastVisibleItem == null) {
+			return 0;
+		}
+		//初始状态下第0个和firstVisible一样的
+		//但是滑动到下面之后第0个其实是firstVisible的上一个item
+		int firstVisiblePosition = listView.getFirstVisiblePosition();
+		int top = lastVisibleItem.getTop();
+		//综合计算一下就可以得出当前Y，不过只限于Item的高度都是一样的情况下
+		int scrollY = -top + firstVisiblePosition * lastVisibleItem.getHeight();
+
+		if (listView instanceof ListView) {
+			ListView view = (ListView) listView;
+			int dividerHeight = view.getDividerHeight();
+			scrollY += dividerHeight * firstVisiblePosition;
+		}
+		return scrollY;
 	}
 }
