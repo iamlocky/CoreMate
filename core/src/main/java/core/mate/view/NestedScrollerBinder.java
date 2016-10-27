c26e384937fcdd4fc22a05dd9fe4bd870736f614
DrkCore <178456643@qq.com>
2016-10-28 00:47:37 +0800

    完善部分方法

--- core/src/main/java/core/mate/view/NestedScrollerBinder.java
+++ core/src/main/java/core/mate/view/NestedScrollerBinder.java
@@ -1,13 +1,14 @@
 package core.mate.view;
 
 import android.support.v4.widget.NestedScrollView;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * @author DrkCore
  * @since 2016-10-27
  */
-public class NestedScrollerBinder extends ScrollBinder implements NestedScrollView.OnScrollChangeListener {
+public class NestedScrollerBinder extends ScrollBinder implements NestedScrollView.OnScrollChangeListener, View.OnTouchListener {
 
     public NestedScrollerBinder(View view) {
         super(view);
@@ -21,8 +22,50 @@ public class NestedScrollerBinder extends ScrollBinder implements NestedScrollVi
         super(view, outY, factor);
     }
 
+    public void bind(NestedScrollView scrollView) {
+        scrollView.setOnScrollChangeListener(this);
+        scrollView.setOnTouchListener(this);
+    }
+
     @Override
     public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
         onScroll(scrollY);
+    }
+
+    public static final long DEFAULT_FINISH_DELAY = 256L;
+
+    private long touchUpFinishDelay = DEFAULT_FINISH_DELAY;
+
+    public NestedScrollerBinder disableTouchUpFinish() {
+        this.touchUpFinishDelay = 0;
+        return this;
+    }
+
+
+    /**
+     * 设置touch事件up后用于调用{@link #onDelayFinishScroll(long)}的延迟。
+     * <p>
+     * 当其小于等于零表示不启用该事件。
+     *
+     * @param touchUpFinishDelay
+     * @return
+     */
+    public NestedScrollerBinder setTouchUpFinish(long touchUpFinishDelay) {
+        this.touchUpFinishDelay = touchUpFinishDelay;
+        return this;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                if (touchUpFinishDelay > 0) {
+                    onDelayFinishScroll(touchUpFinishDelay);
+                }
+                break;
+        }
+        return false;
     }
 }
