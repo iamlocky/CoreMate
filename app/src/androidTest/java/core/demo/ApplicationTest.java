cf9f67f6ffdf3516c071b5839a950db1f02d4563
DrkCore <178456643@qq.com>
2017-04-03 08:41:15 +0800

    移除测试目录

--- app/src/androidTest/java/core/demo/ApplicationTest.java
+++ app/src/androidTest/java/core/demo/ApplicationTest.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.test.ApplicationTestCase;
 
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Funasddamentals</a>
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Funasd123damentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
